c33830694ca6b58c98d56c752263748e501789f3
Burton Barnes <barnesburton@gmail.com>
2022-09-08 15:25:47 -0500

    exercise completed

---
+++ tables_exercises.sql
@@ -0,0 +1,21 @@
+# Question 3
+#USE employees;
+
+# Question 4
+#SELECT *
+
+#Question 5: Numeric, String, and Date types are used
+#Question 6: Which table(s) do you think contain a numeric type column? I believe that salaries has numeric type columns
+#Question 7: Which table(s) do you think contain a string type column? I believe that deapartments and titles have string columns
+#Question 8: Which table(s) do you think contain a date type column? I believe that dept_emp and dept_manager has date columns
+#Question 9: What is the relationship between the employees and the departments tables? 
+#Answer: Employees are divided into different departments making it easier to group them. This is not a direct relationship by are related by a table
+
+#Question 10
+USE employees;
+
+SELECT *;
+USE Tables;
+
+SELECT *
+FROM dept_manager;
