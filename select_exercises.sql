b244252beea0a9e7fe187a00bfbcf20bb2b35cc8
Burton Barnes <barnesburton@gmail.com>
2022-09-08 15:29:18 -0500

    completed exercise

---
+++ select_exercises.sql
@@ -0,0 +1,24 @@
+SELECT *
+FROM albums;
+DESCRIBE albums;
+
+#Question 3
+#a. 31 rows 
+#b. 23 unique artist names
+#c. the primary key is id
+#d. Oldest release date is 1967, most recent is 2011
+
+#4.a.SELECT *
+#FROM albums
+#WHERE artist = 'Pink Floyd'
+
+#b. WHERE name = "Sgt. Pepper's Lonely Hearts Club Band", 1967
+
+#c. WHERE name = 'Nevermind'   Grunge, alternative rock
+
+#d. WHERE release_date < 2000 AND release_date >= 1990
+
+#e. WHERE sales < 20
+
+#f. WHERE genre = 'Rock', it doesn't have Hard or Progressive Rock because it is different from Rock.
+# It doesn't fit the parameters of the queries
